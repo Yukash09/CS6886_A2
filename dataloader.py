b1bf5a8258af483a364150eef9ba25fdf6e90da1
Yukash09 <yukayugan@gmail.com>
2026-09-03 23:58:37 +0530

    Fine-tune MobileNet

---
+++ dataloader.py
@@ -0,0 +1,41 @@
+import torch 
+import torch.nn as nn 
+import torchvision
+from torchvision import datasets
+from torchvision import transforms
+from torch.utils.data import DataLoader , random_split
+from typing import Any
+
+def get_test_data(batch_size: int = 128) :
+
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ])
+
+    test_data = datasets.CIFAR10(root="./data" , train=False, download=True, transform=test_transform)
+    print(f"Test Data len: {len(test_data)}")
+
+    test_loader = DataLoader(dataset=test_data, batch_size=batch_size, shuffle=False)
+
+    return test_loader 
+
+def get_train_data(batch_size:int = 128) :
+
+    train_transform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ])
+
+    data = datasets.CIFAR10(root="./data", train=True, download=True, transform=train_transform)
+    generator = torch.Generator().manual_seed(3)
+    train_data , val_data = random_split(data, [int(0.85*len(data)) , len(data) - int(0.85*len(data))] , generator)
+
+    print(f"Train and Val Data len: {len(train_data)} and {len(val_data)}")
+
+    train_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(dataset=val_data , batch_size=batch_size , shuffle=False)
+
+    return train_loader , val_loader
---
+++ train.py
@@ -0,0 +1,117 @@
+import torch.nn as nn 
+import torch
+import torch.optim as optim
+from model import get_model
+from dataloader import get_train_data
+import wandb 
+
+
+def train():
+    wandb.init(project="CS6886_Assignment2")
+    cfg = wandb.config
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
+    model = get_model().to(device)
+
+    train_data , val_data = get_train_data(batch_size=cfg.batch_size)
+
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters() , lr=cfg.lr)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max' , factor=0.5 , patience=3)
+
+    best_acc = 0.0 
+
+    for epoch in range(cfg.epoch):
+        print(f"Epoch: {epoch}")
+
+        model.train()
+        epoch_loss = 0.0 
+        correct = 0 
+        total = 0 
+
+        for idx , (images , labels) in enumerate(train_data):
+            images = images.to(device)
+            labels = labels.to(device)
+            logits = model(images)
+            _ , pred = torch.max(logits , 1)
+            loss = loss_fn(logits , labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            epoch_loss += loss.item()
+            correct += (pred == labels).sum().item()
+            total += labels.size(0)
+
+            if (idx + 1) % 20 == 0:
+                print(f"Training Batch {idx}/{len(train_data)} , Loss: {loss.item():.4f}")
+        
+        train_accuracy = correct/total 
+        train_loss = epoch_loss/len(train_data)
+
+        print(f"Train Accuracy:{train_accuracy:.4f} , Train_loss:{train_loss:.4f}")
+
+        model.eval()
+        epoch_val_loss = 0.0 
+        correct_val = 0 
+        total_val = 0 
+
+        with torch.no_grad():
+            for idx , (images , labels) in enumerate(val_data):
+                images = images.to(device)
+                labels = labels.to(device)
+                logits = model(images)
+                _ , pred = torch.max(logits , 1)
+                val_loss = loss_fn(logits , labels)
+
+                epoch_val_loss += val_loss.item()
+                correct_val += (pred == labels).sum().item()
+                total_val += labels.size(0)
+
+                if (idx + 1) % 20 == 0:
+                    print(f"Validation Batch {idx}/{len(val_data)} , Loss: {epoch_val_loss:.4f}")
+        
+        val_accuracy = correct_val/total_val 
+        valid_loss = epoch_val_loss/len(val_data)
+
+        print(f"Train Accuracy:{val_accuracy:.4f} , Train_loss:{valid_loss:.4f}")
+
+        scheduler.step(val_accuracy)
+
+        wandb.log({
+            "epoch":epoch , 
+            "train_loss": train_loss,
+            "val_loss": valid_loss ,
+            "train_acc": train_accuracy,
+            "val_acc": val_accuracy,
+            "lr": optimizer.param_groups[0]['lr']
+        })
+
+        if val_accuracy > best_acc:
+            best_acc = val_accuracy 
+            checkpoint = {
+                "state_dict": model.state_dict() , 
+                "epoch": epoch ,
+                "best_metric": best_acc
+            }
+            torch.save(checkpoint , "./checkpoints/best_model.pth")
+
+
+if __name__ == "__main__":
+    config = {
+        'method': 'bayes',
+        'name': "Fine-Tuning-Sweep",
+        'metric':{
+            'goal':'maximize',
+            'name':'val_acc'
+        },
+        'parameters':{
+            'lr':{'values': [0.001, 0.0005, 0.0001]},
+            'batch_size':{'values':[64 , 128]},
+            'epoch':{'values':[15 , 25]}
+        }
+    }
+
+    sweep_id = wandb.sweep(config, project="CS6886_Assignment2")
+    wandb.agent(sweep_id, function=train , count=12) # Don't let bayes run forever?
