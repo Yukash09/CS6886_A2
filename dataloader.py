cb94247cbf24ab39af222b3a02770ff1eae6b414
Yukash09 <yukayugan@gmail.com>
2026-09-04 21:46:46 +0530

    Fix tensor transform

--- dataloader.py
+++ dataloader.py
@@ -26,8 +26,8 @@ def get_train_data(batch_size:int = 128) :
     train_transform = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
         transforms.AutoAugment(transforms.AutoAugmentPolicy.CIFAR10),
+        transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
     ])
 
